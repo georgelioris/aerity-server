c4cff2899d1c2fc26c2b10c61a10dc04ff55d774
George Lioris <george.lioris@gmail.com>
2020-01-12 05:02:34 +0200

    Added mongoose schema to store api responses

---
+++ models/weatherData.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const weatherDataSchema = new Schema({
+  _id: {
+    type: String,
+    requred: true
+  },
+  locationId: {
+    type: String,
+    requred: true
+  },
+  data: {
+    type: String,
+    required: true
+  },
+  clientID: {
+    type: String,
+    required: true
+  },
+  dataDate: {
+    type: Number,
+    requred: true,
+    default: Date.now()
+  },
+  expired: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+module.exports = model('savedData', weatherDataSchema);
