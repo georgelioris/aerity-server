169993af6d17fbe5bb21b53260ab12c9482862d9
George Lioris <george.lioris@gmail.com>
2020-01-13 02:05:50 +0200

    Added helmet and compression

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 
 mongoose.connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
@@ -14,6 +16,8 @@ db.on('error', e => console.log(e));
 db.once('open', () =>
   console.log(`Connected to Database on ${process.env.DATABASE_URL}`)
 );
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 const weatherDataRouter = require('./routes/weatherDataRouter');
